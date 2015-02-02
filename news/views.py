fbc5fbc7a5fa6934378df15939bd4f6a32ddceba
Jeremy Leipzig <leipzig@gmail.com>
2015-02-02 14:44:56 -0500

    feeds

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.list import ListView
+from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
